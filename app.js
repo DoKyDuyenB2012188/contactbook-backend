1c028995250ec49a747a4d852925ef526059c768
dokyduyen <84983247+dokyduyen@users.noreply.github.com>
2022-09-22 16:13:02 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", contactsRouter);
+const ApiError = require("./app/api-error");
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
+});
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+  return res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
 });
 
 module.exports = app;
